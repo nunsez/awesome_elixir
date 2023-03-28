f2a82608c054941d8db4d79fc5d775085ff7d903
Alexander Mandrikov <mandrikov@pm.me>
2023-03-28 13:56:40 +0700

    add ecto models and migrations for category and library

---
+++ services/backend/apps/awesome_elixir/lib/awesome_elixir/library.ex
@@ -0,0 +1,34 @@
+defmodule AwesomeElixir.Library do
+  @moduledoc false
+
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias AwesomeElixir.Repo
+  alias AwesomeElixir.Category
+
+  @type t :: %__MODULE__{}
+
+  schema "libraries" do
+    field :description, :string
+    field :last_commit, :utc_datetime
+    field :name, :string
+    field :stars, :integer
+    belongs_to :category, Category
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(library, attrs) do
+    library
+    |> cast(attrs, [:name, :description, :stars, :last_commit])
+    |> validate_required([:name, :description, :stars, :last_commit])
+  end
+
+  def insert(attrs) do
+    %__MODULE__{}
+    |> changeset(attrs)
+    |> Repo.insert()
+  end
+end
