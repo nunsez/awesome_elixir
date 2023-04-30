a9a11d53aa9b415201771f0e2d3ec83d318359b9
Alexander Mandrikov <mandrikov@pm.me>
2023-04-30 16:05:45 +0700

    update page_html

--- lib/awesome_elixir_web/controllers/page_html.ex
+++ lib/awesome_elixir_web/controllers/page_html.ex
@@ -1,71 +1,41 @@
 defmodule AwesomeElixirWeb.PageHTML do
-  alias AwesomeElixir.Category
   use AwesomeElixirWeb, :html
+
+  alias AwesomeElixir.Category
+  alias AwesomeElixir.Delimiter
+  alias AwesomeElixir.Library
+  alias AwesomeElixirWeb.Gettext
+  alias Phoenix.LiveComponent
 
   embed_templates "page_html/*"
 
-  def home(assigns) do
-    ~H"""
-    <.category_list items={@categories} />
-    <.library_list categories={@categories} />
-    """
-  end
+  attr :categories, :list, default: []
 
-  def home_solid(assigns) do
-    ~H"""
-    <div id="app"></div>
-    """
-  end
-
-  attr :items, :list, required: true
-
-  def category_list(assigns) do
-    ~H"""
-    <.header>List of Categories</.header>
-    <.list>
-      <:item :for={item <- @items} title={item.name}>
-        <%= item.description %>
-      </:item>
-    </.list>
-    """
-  end
-
-  attr :items, :list, required: true
-
-  def category_list_old(assigns) do
-    ~H"""
-    <ul>
-    <%= for item <- @items do %>
-      <.category item={item} />
-    <% end %>
-    </ul>
-    """
-  end
-
-  attr :item, Category, required: true
-
-  def category(assigns) do
-    ~H"""
-    <li>
-      <%= @item.name %>: <%= @item.name %>
-    </li>
-    """
-  end
+  @spec category_list(assigns :: map()) :: LiveComponent.t()
+  def category_list(assigns)
 
   attr :categories, :list, default: []
 
-  def library_list(assigns) do
-    ~H"""
-    <.header>List of Libraries</.header>
+  @spec library_list(assigns :: map()) :: LiveComponent.t()
+  def library_list(assigns)
 
-    <%= for category <- @categories do %>
-      <div><%= category.name %></div>
-        <.list>
-          <:item :for={library <- category.libraries} title={library.name}>
-            <%= library.description %> -- <%= library.stars %> -- <%= library.last_commit %>
-          </:item>
-        </.list>
-    <% end %>
-    """
+  @spec category_id(category :: Category.t()) :: String.t()
+  def category_id(category), do: "category-#{category.id}"
+
+  @spec category_anchor(category :: Category.t()) :: String.t()
+  def category_anchor(category), do: "#" <> category_id(category)
+
+  @spec days_from_last_commit(library :: Library.t()) :: integer()
+  def days_from_last_commit(%Library{} = library) do
+    DateTime.utc_now()
+    |> DateTime.diff(library.last_commit, :day)
+  end
+
+  @spec delimit(number :: integer()) :: String.t()
+  def delimit(number), do: Delimiter.call(number, " ")
+
+  @spec days(number :: integer()) :: String.t()
+  def days(number) do
+    Gettext.ngettext("day", "days", number)
   end
 end
