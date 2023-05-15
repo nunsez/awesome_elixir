f22d638fee6dfde3208187c7a92899b29c2a14b5
Alexander Mandrikov <mandrikov@pm.me>
2023-05-15 16:58:59 +0700

    add aliases to mix.exs

--- mix.exs
+++ mix.exs
@@ -70,7 +70,10 @@ defmodule AwesomeElixir.MixProject do
       setup: ["deps.get", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      "assets.setup": ["npm install"],
+      "assets.build": ["npm run build"],
+      "assets.deploy": ["npm run build", "phx.digest"]
     ]
   end
 
